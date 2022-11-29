f4c2644eb6f90955afd66393a747f1a0491dd61a
DeKoss97 <109168125+DeKoss97@users.noreply.github.com>
2022-11-29 13:07:00 +0200

    check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,37 +1,19 @@
 #include "lists.h"
-
 #include <stdio.h>
 
-
-
 int check_cycle(listint_t *list)
-  
 {
-  
-  listint_t *cursor;
-  
-  listint_t *head;
-  
+	listint_t *cursor;
+	listint_t *head;
 
-  
-  head = list;
-  
-  cursor = list;
-  
-  while (cursor != NULL && cursor->next != NULL)
-    
-    {
-      
-      head = head->next;
-      
-      cursor = cursor->next->next;
-      
-      if (head == cursor)
-	
-	return (1);
-      
-    }
-  
-  return (0);
-  
+	head = list;
+	cursor = list;
+	while (cursor != NULL && cursor->next != NULL)
+	{
+		head = head->next;
+		cursor = cursor->next->next;
+		if (head == cursor)
+			return (1);
+	}
+	return (0);
 }
