f311b8979b523f39f2ee252264023825c6899110
DeKoss97 <109168125+DeKoss97@users.noreply.github.com>
2022-11-29 20:50:34 +0200

    100-print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 is 0:
-        num = i
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
     else:
-        num = i - 32
-    print("{:s}".format(chr(num)), end="")
+        print('{:c}'.format(i - 32), end='')
+        
